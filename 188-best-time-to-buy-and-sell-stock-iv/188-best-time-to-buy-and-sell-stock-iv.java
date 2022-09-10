adba92853b19978693935496a51f68c05d1df26d
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2022-09-11 00:14:41 +0300

    Time: 14 ms (22.58%), Space: 42.6 MB (51.55%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,8 +1,9 @@
 class Solution {
 	public int maxProfit(int k, int[] prices) {
-		if (k >= prices.length / 2)
-			return maxProfit(prices); // This is to avoid Memory Limit Exceeded for one of the test cases. In other words, you can make infinite transactions. Using variant 2 logic. 
-		int dp[][] = new int[k + 1][prices.length];
+	if(prices.length ==0)
+            return 0;
+       //int k=2;
+       int dp[][] = new int[k + 1][prices.length];
 		for (int i = 1; i < dp.length; i++) {
 			int maxDiff = -prices[0];
 			for (int j = 1; j < dp[0].length; j++) {
@@ -13,13 +14,5 @@ class Solution {
 		return dp[k][prices.length - 1];
 	}
 
-	private int maxProfit(int[] prices) {
-        int profit = 0;
-		for (int i = 0; i < prices.length - 1; i++) {
-			if (prices[i + 1] > prices[i]) {
-				profit += prices[i+1] - prices[i];
-			}
-		}
-		return profit;
-    }
+
 }

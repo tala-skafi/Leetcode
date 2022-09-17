6a55b35afb16543f16d0294c94da0096d5d9ed7d
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2022-09-17 17:07:06 +0300

    Time: 48 ms (83.95%), Space: 130.8 MB (55.97%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,14 +1,33 @@
 class Solution {
-   public int[] findOriginalArray(int[] changed) {
-    int[] temp = new int[changed.length/2];
-    Queue<Integer> x = new LinkedList<>();
-    Arrays.sort(changed);
-    int i = 0;
-    for(int num : changed){
-        if(!x.isEmpty() && x.peek()==num)
-            temp[i++] = x.poll()/2;
-        else x.add(num*2);
+    public int[] findOriginalArray(int[] changed) {
+        
+        int n = changed.length;
+        int []vis = new int[(int)1e5+1];
+        
+        if( n%2!=0 ) return new int[0];
+        int []ans = new int[n/2];
+        int j = 0;
+        
+		// filling values in frequency array
+        for( int i=0; i<n; i++ ){
+            int val = changed[i];
+            vis[val]++;
+        }
+        
+        for( int i=0; i<vis.length; i++ ){
+			// loop to make the frequency of current element zero
+            while( vis[i]>0 ){
+                if( i*2<vis.length && vis[i*2]>0 ) {
+                    ans[j++] = i;
+                    vis[i*2]--;
+                }
+				// if we cant find the double value for any frequency of ith element return empty array
+                else return new int[0];
+                
+                vis[i]--;
+            }
+        }
+        
+        return ans;
     }
-    return x.size()>0 ? new int[]{}:temp;
-}
 }

86e6ec05c5ad4fc47c957c5639b0eda08b688883
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2022-10-10 23:50:29 +0300

    Time: 8 ms (32.93%), Space: 54.2 MB (7.23%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int flag=0;
+     HashSet<Integer> set;
+    public boolean findTarget(TreeNode root, int k) {
+       set=new HashSet<>();
+        rec(root,k);
+        if(flag==1)return true;
+        return false;
+    }
+    public void rec(TreeNode root, int k){
+        if(root==null)return;
+        if(set.contains(root.val))flag=1;
+        set.add(k-root.val);
+        rec(root.left,k);
+        rec(root.right,k);
+    }
+}

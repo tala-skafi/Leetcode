7c2ec1be376b1e3697dca04015ed046b255cbd25
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2022-10-20 20:14:24 +0300

    Time: 4 ms (52.57%), Space: 55.9 MB (15.78%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -10,25 +10,34 @@
  * }
  */
 public class Solution {
-    int flag=0;
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-       ListNode temp=headB;
-       while(headA!=null){
-            while (headB!=null){
-                if(headA.next==headB.next ){
-                    if(headA==headB){
-                        return headA;
-                    }
-                   return headA.next;
-                     }
-                headB=headB.next;
-            }
-           
-         headB=temp;  
-         headA=headA.next; 
-       }
-        return null;
+public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    ListNode p1 = headA, p2 = headB;
+    int len1 = 0, len2 = 0;
+    while (p1 != null) {
+        p1 = p1.next;
+        len1++;
     }
+    while (p2 != null) {
+        p2 = p2.next;
+        len2++;
+    }
+    p1 = headA;
+    p2 = headB;
+    if (len1 > len2) {
+        for (int i = 0;i < len1 - len2; i++) {
+            p1 = p1.next;
+        }
+    } else {
+        for (int i = 0;i < len2 - len1; i++) {
+            p2 = p2.next;
+        }
+    }
+    while (p1 != p2) {
+        p1 = p1.next;
+        p2 = p2.next;
+    }
+    return p1;
+}
 }
 
 

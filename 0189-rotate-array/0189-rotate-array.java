6d5b88984d6f722da61ec6d7d0c3fee675015a74
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2023-01-20 16:20:06 +0000

    Time: 1 ms (73.60%), Space: 57.8 MB (94.66%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,31 @@
+import java.lang.Math;
+class Solution {
+    public void rotate(int[] nums, int k) {
+
+        k= k%nums.length;
+        int limit=nums.length-k;
+        int index=0;
+        int temp;
+        int[] result=new int[nums.length];
+        
+          for(int i=nums.length-1;i>=0;i--){
+            if(i>=limit){
+                 index=i-limit;
+                 result[index]=nums[i];    
+            }
+            else{
+                    index=k+i;
+                     result[index]=nums[i];
+            }
+            
+        }
+        for(int i=0;i<result.length;i++){
+             nums[i]= result[i];
+        }
+            
+        
+       
+
+        
+    }
+}

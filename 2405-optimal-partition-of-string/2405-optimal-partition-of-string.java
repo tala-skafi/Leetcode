47a19882606b237cff507489b9b58de8650e9882
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2023-04-05 21:36:50 +0300

    Time: 39 ms (36.39%), Space: 43 MB (67.78%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -2,13 +2,13 @@ class Solution {
     public int partitionString(String s) {
         int idx = 0;
         int count = 0;
-        Map<Character, Boolean> mp = new HashMap<>(); // use a HashMap instead of an unordered_map
+       HashSet<Character> mp = new HashSet<>(); // use a HashMap instead of an unordered_map
         while (idx < s.length()) {
-            if (mp.containsKey(s.charAt(idx))) { // if current character has already appeared in current substring
+            if (mp.contains(s.charAt(idx))) { // if current character has already appeared in current substring
                 count++; // increment count
                 mp.clear(); // clear HashMap to start a new substring
             }
-            mp.put(s.charAt(idx), true); // mark current character as seen
+            mp.add(s.charAt(idx)); // mark current character as seen
             idx++; // move to next character
         }
         return count + 1; // return number of substrings (add 1 because we need to count the last substring as well)

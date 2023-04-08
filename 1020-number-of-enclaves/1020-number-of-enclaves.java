c832ef0444ca2f2daa56a5279c23591af37fbf94
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2023-04-08 03:05:37 +0300

    Time: 8 ms (92.81%), Space: 55.2 MB (45.94%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -2,25 +2,37 @@ class Solution {
     public int numEnclaves(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+        
+        // mark all boundary land cells and their connected cells
+        for (int i = 0; i < m; i++) {
+            dfs(grid, i, 0);
+            dfs(grid, i, n-1);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(grid, 0, j);
+            dfs(grid, m-1, j);
+        }
+        
+        // count unmarked land cells
+        int count = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if ((i == 0 || j == 0 || i == m - 1 || j == n - 1) && grid[i][j] == 1) {
-                    dfs(grid, i, j);
+                if (grid[i][j] == 1) {
+                    count++;
                 }
             }
         }
-        return Arrays.stream(grid).mapToInt(row -> Arrays.stream(row).sum()).sum();
+        return count;
     }
     
     private void dfs(int[][] grid, int i, int j) {
-        grid[i][j] = 0;
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        for (int[] direction : directions) {
-            int x = i + direction[0];
-            int y = j + direction[1];
-            if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] == 1) {
-                dfs(grid, x, y);
-            }
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != 1) {
+            return;
         }
+        grid[i][j] = 0; // mark as visited
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
     }
 }

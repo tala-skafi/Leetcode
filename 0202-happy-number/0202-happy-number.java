ba017132e53f9c08202ef34ce6951da26ef76503
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2024-02-20 20:20:27 +0300

    Time: 0 ms (100.00%), Space: 40.7 MB (56.97%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isHappy(int num) {
+          int slow = num, fast = num;
+    do {
+      slow = findSquareSum(slow); // move one step
+      fast = findSquareSum(findSquareSum(fast)); // move two steps
+    } while (slow != fast); // found the cycle
+
+    return slow == 1; // see if the cycle is stuck on the number '1'
+        
+    }
+      private  int findSquareSum(int num) {
+    int sum = 0, digit;
+    while (num > 0) {
+      digit = num % 10;
+      sum += digit * digit;
+      num /= 10;
+    }
+    return sum;
+  }
+}

b7bbd2a8efc9d47720d385ab8f945af175d56968
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2024-05-08 21:00:28 +0300

    Time: 289 ms (5.23%) | Memory: 83.3 MB (8.75%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        int[]bucket=new int[10000000];
+        for(int i=0;i<score.length;i++){
+            bucket[score[i]]=i+1;
+
+        }
+        String[] output=new String[score.length];
+        int counter=1;
+        for(int i=bucket.length-1;i>=0;i--){
+            if(bucket[i]>0){
+                switch(counter) {
+                        case 1:
+                             output[bucket[i]-1]="Gold Medal";
+                             break;
+                        case 2:
+                             output[bucket[i]-1]="Silver Medal";
+                             break;
+                        case 3:
+                             output[bucket[i]-1]="Bronze Medal";
+                             break;
+                        default:
+                            output[bucket[i]-1]=String.valueOf(counter);
+                }
+                counter++;
+            }
+
+        }
+        return output;
+        
+    }
+}

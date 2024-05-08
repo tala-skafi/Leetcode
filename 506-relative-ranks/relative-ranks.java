257d921810c1e92d70e0ea6da74156567bdec4ea
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2024-05-08 21:06:10 +0300

    Time: 2 ms (100.00%) | Memory: 45.5 MB (58.39%) - LeetSync

--- 506-relative-ranks/relative-ranks.java
+++ 506-relative-ranks/relative-ranks.java
@@ -1,15 +1,16 @@
 class Solution {
     public String[] findRelativeRanks(int[] score) {
-        int[]bucket=new int[1000000];
         int max=0;
         for(int i=0;i<score.length;i++){
-            bucket[score[i]]=i+1;
             max=Math.max(max,score[i]);
-
+        }
+        int[]bucket=new int[max+1];
+        for(int i=0;i<score.length;i++){
+            bucket[score[i]]=i+1;
         }
         String[] output=new String[score.length];
         int counter=1;
-        for(int i=max+1;i>=0;i--){
+        for(int i=bucket.length-1;i>=0;i--){
             if(bucket[i]>0){
                 switch(counter) {
                         case 1:

09e946690637bc87556cdca4de916c773850bc69
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2024-05-14 22:58:22 +0300

    Time: 730 ms (12.20%) | Memory: 68.4 MB (39.75%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.java
@@ -21,7 +21,6 @@ class Solution {
         int[] pair=new int[]{0,0};
            while (!maxHeap.isEmpty() &&k>0) {
              pair = maxHeap.poll();
-            System.out.println("(" + pair[0] + ", " + pair[1] + ")");
             k--;
         
         }

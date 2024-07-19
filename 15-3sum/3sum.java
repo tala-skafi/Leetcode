db5659441430629ff2574e4cb4abb21851341051
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2024-07-20 01:40:27 +0300

    Time: 24 ms (97.92%) | Memory: 51.4 MB (56.38%) - LeetSync

---
+++ 15-3sum/3sum.java
@@ -0,0 +1,31 @@
+class Solution {
+    List<List<Integer>> output;
+    public List<List<Integer>> threeSum(int[] nums) {
+        output=new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+           if(i>0 && nums[i]==nums[i-1])continue;
+            find(-nums[i], i+1 , nums.length-1, nums);
+
+
+        }
+        return output;
+        
+    }
+    private void find(int value, int i, int j, int [] nums){
+        while(i<j){
+            if(nums[i]+nums[j]>value)
+            j--;
+            else if(nums[i]+nums[j]<value)
+            i++;
+            else {
+                   output.add(Arrays.asList(-value, nums[i], nums[j]));
+                   while(i<j&&nums[i]==nums[i+1])i++;
+                    while(i<j&&nums[j]==nums[j-1])j--;
+                    i++;
+                    j--;
+                
+            }
+        }
+    }
+}

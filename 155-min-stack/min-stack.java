01ebae1397dd1dd63d5230cf6c57d8bdc8e03fa0
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2024-07-20 02:37:35 +0300

    Time: 4 ms (95.65%) | Memory: 44.6 MB (61.45%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,34 +1,40 @@
 class MinStack {
-	private Node head;
-        
+    int min;
+    Stack<Integer> stack;
+    public MinStack() {
+        stack = new Stack<Integer>();
+         min = Integer.MAX_VALUE;
+    }
     public void push(int x) {
-        if (head == null) 
-            head = new Node(x, x, null);
-        else 
-            head = new Node(x, Math.min(x, head.min), head);
-    }
-    
-    public void pop() {
-        head = head.next;
-    }
-    
-    public int top() {
-        return head.val;
-    }
-    
-    public int getMin() {
-        return head.min;
-    }
-        
-    private class Node {
-        int val;
-        int min;
-        Node next;
-            
-        private Node(int val, int min, Node next) {
-            this.val = val;
-            this.min = min;
-            this.next = next;
+        // only push the old minimum value when the current 
+        // minimum value changes after pushing the new value x
+        if(x <= min){          
+            stack.push(min);
+            min=x;
         }
+        stack.push(x);
+    }
+
+    public void pop() {
+        // if pop operation could result in the changing of the current minimum value, 
+        // pop twice and change the current minimum value to the last minimum value.
+        if(stack.pop() == min) min=stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */

7de5d2b7fde489aedd45e06d8b58d2052c23f4cd
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2024-07-20 14:15:29 +0300

    Time: 6 ms (70.87%) | Memory: 45.1 MB (9.81%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -3,13 +3,10 @@ class Solution {
         Deque<Integer> stack = new ArrayDeque<>();
         int op1, op2;
         for (String token : tokens) {
-            if (token.equals("-")){
+            if (token.equals("-")) {
                 int sub = stack.pop();
-                stack.push(stack.pop()- sub);
-
-            }
-
-            else if (token.equals("+"))
+                stack.push(stack.pop() - sub);
+            } else if (token.equals("+"))
                 stack.push(stack.pop() + stack.pop());
 
             else if (token.equals("*"))

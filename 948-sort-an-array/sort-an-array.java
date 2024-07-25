b4b46e13b622ef6d33d2563c9ca696cf6fddf065
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2024-07-25 15:35:27 +0300

    Time: 26 ms (65.17%) | Memory: 56.7 MB (29.56%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+         if (nums == null || nums.length == 0) return new int[]{};
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+        
+    }
+     private void mergeSort(int[] nums, int l, int r) {
+        if (l >= r) return;
+        int mid = l + (r - l) / 2;
+        mergeSort(nums, l, mid);
+        mergeSort(nums, mid + 1, r);
+        merge(nums, l, r);
+    }
+    private void merge(int[] nums, int l, int r) {
+        int mid = l + (r - l) / 2;
+        int[] tmp = new int[r - l + 1];
+        int i = l, j = mid + 1, k = 0;
+        while (i <= mid || j <= r) {
+            if (i > mid || j <= r && nums[i] > nums[j]) {
+                tmp[k++] = nums[j++];
+            } else {
+                tmp[k++] = nums[i++];
+            }
+        }
+        System.arraycopy(tmp, 0, nums, l, r - l + 1);
+    }
+}

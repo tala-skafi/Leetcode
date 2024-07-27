17147f74282b8de2a8ceb1c4aee4f6ef55a479fc
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2024-07-27 20:15:34 +0300

    Time: 14 ms (24.98%) | Memory: 44.4 MB (48.54%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,61 +1,46 @@
 class Solution {
-    char [][] b;
 
     public boolean isValidSudoku(char[][] board) {
-        b = board;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-               if(board[i][j]!='.') if (!checkValid(i, j))
-                    return false;
+        Set <Integer> set=new HashSet<>();
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.')continue;
+            int val=Character.getNumericValue(board[i][j]);
+            if(set.contains(val))return false;
+            set.add(val);
             }
+            set.clear();
+            
         }
-        return true;
-
-    }
-
-    private boolean checkValid(int i, int j) {
-        int val=Character.getNumericValue(b[i][j]);
-        return checkRow(val, i, j) && checkColumn(val, i, j) && checkSubBoard(val, i, j);
-
-    }
-
-    private boolean checkRow(int val, int r, int c) {
-        for (int i = 0; i < 9; i++) {
-            if (Character.isLetterOrDigit(b[r][i])&&Character.getNumericValue(b[r][i]) == val && i != c)
-                return false;
-        }
-        return true;
-    }
-
-    private boolean checkColumn(int val, int r, int c) {
-        for (int i = 0; i < 9; i++) {
-            if (Character.isLetterOrDigit(b[i][c])&&Character.getNumericValue(b[i][c]) == val && i != r)
-                return false;
-        }
-        return true;
-
-    }
-
-    private boolean checkSubBoard(int val, int r, int c) {
-        int i = findStart(r);
-        int j = findStart(c);
-        int endi = i + 3;
-        int endj = j + 3;
-        for (int out = i; out < endi; out++) {
-            for (int inner = j; inner < endj; inner++) {
-                if (Character.isLetterOrDigit(b[out][inner])&&Character.getNumericValue(b[out][inner]) == val && (out != r && inner != c))
-                    return false;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[j][i]=='.')continue;
+            int val=Character.getNumericValue(board[j][i]);
+            if(set.contains(val))return false;
+            set.add(val);
             }
-        }
-        return true;
+            set.clear();
+            
+                }
+    set.clear();
 
+
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                int newI=3*(i/3);
+                int newJ=3*(i%3);
+                char c=board[newI+j/3][newJ+j%3];
+                if(c=='.')continue;
+                System.out.print(c);
+                int val=Character.getNumericValue(c);
+                if(set.contains(val))return false;
+                set.add(val);
+            }
+            System.out.println("______________");
+            set.clear();
+        } 
+       return true;
+        
+        
     }
-
-    private int findStart(int v){
-        if(v>=0 && v<3)return 0;
-        if(v>=3 && v<6 ) return 3;
-        if(v>=6) return 6;
-        else return -1;
-    }
-
 }

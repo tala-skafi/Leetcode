ad8903a0079ef134fc4f73462abc29d54e32e068
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2024-07-27 20:15:49 +0300

    Time: 3 ms (59.25%) | Memory: 44.7 MB (27.33%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -22,21 +22,16 @@ class Solution {
             set.clear();
             
                 }
-    set.clear();
-
-
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
                 int newI=3*(i/3);
                 int newJ=3*(i%3);
                 char c=board[newI+j/3][newJ+j%3];
                 if(c=='.')continue;
-                System.out.print(c);
                 int val=Character.getNumericValue(c);
                 if(set.contains(val))return false;
                 set.add(val);
             }
-            System.out.println("______________");
             set.clear();
         } 
        return true;

fbc0ba691a290775610af6272c01e2489f33ec9f
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2024-08-04 19:50:10 +0300

    Time: 329 ms (5.07%) | Memory: 46.4 MB (6.00%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,31 +1,27 @@
 class Solution {
     Set<Pair<Integer, Integer>> solutions;
     Set<Integer> visited;
-    Map <Integer,List<Integer>> map;
+    Map<Integer, List<Integer>> map;
 
     public int[] findRedundantConnection(int[][] edges) {
         int n = edges.length;
         visited = new HashSet<>();
         solutions = new HashSet<>();
         map = new HashMap<>();
-        for(int[]e:edges){
-            int s=e[0];
-            int d=e[1];
-            if(!map.containsKey(s)){
-                map.put(s,new ArrayList<>());
+        for (int[] e : edges) {
+            int s = e[0];
+            int d = e[1];
+            if (!map.containsKey(s)) {
+                map.put(s, new ArrayList<>());
             }
             map.get(s).add(d);
-            if(!map.containsKey(d)){
-                map.put(d,new ArrayList<>());
+            if (!map.containsKey(d)) {
+                map.put(d, new ArrayList<>());
             }
             map.get(d).add(s);
-        } // Printing the map
-        for (Map.Entry<Integer, List<Integer>> entry : map.entrySet()) {
-            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
         }
-
         for (int i = 1; i <= n; i++) {
-             visited.clear();
+            visited.clear();
             DFS(i, 0);
         }
         for (int i = n - 1; i >= 0; i--) {
@@ -35,19 +31,19 @@ class Solution {
         return new int[] { 0, 0 };
     }
 
-    private void DFS(int node, int parent){
-        if(visited.contains(node)){
-             System.out.println("I'm in "+node+" and parent is "+ parent);
-            solutions.add(new Pair(node,parent));
+    private void DFS(int node, int parent) {
+        if (visited.contains(node)) {
+            solutions.add(new Pair(node, parent));
             return;
         }
-       
+
         visited.add(node);
-        List<Integer> neighbours=map.get(node);
-        if(neighbours!=null)
-        for(int n:neighbours){
-            if(n!=parent)DFS(n,node);
-        }
-       
+        List<Integer> neighbours = map.get(node);
+        if (neighbours != null)
+            for (int n : neighbours) {
+                if (n != parent)
+                    DFS(n, node);
+            }
+
     }
 }

e74c7e4d440f3e659afd1d9ef48bb1eb9d2cae8b
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2024-08-05 15:45:12 +0300

    Time: 76 ms (69.46%) | Memory: 46.1 MB (72.68%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,48 +1,51 @@
+
+
 class Solution {
+    static class Pair {
+        String s;
+        int seqLen;
+
+        Pair(String s, int seqLen) {
+            this.s = s;
+            this.seqLen = seqLen;
+        }
+    }
+
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        int level=0;
-        int index=0;
-        Queue<String>p=new LinkedList<>();
-        Queue<String>q=new LinkedList<>();
-        Set<String> set=new HashSet<>();
+        Queue<Pair> q = new LinkedList<>();
+        Set<String> list = new HashSet<>(wordList);
 
-        p.add(beginWord);
-    while(!p.isEmpty()){
-    //1.pop front
-    String front=p.poll();
-    //2.check if goal
-    if(front.equals(endWord)){
-        return level+1;
-    }
-    //3.check neighbours
-    for(int i=0;i<wordList.size();i++){
-      if(!set.contains(wordList.get(i)))
-      if(check(wordList.get(i),front)){
-         q.add(wordList.get(i));
-         set.add(wordList.get(i));
-    }
-    
-}
-    //4.check if level over
-     if(p.isEmpty()){
-      level++;
-       p=q;
-       q=new LinkedList<>();
- }   
-    
-    
+        if (!list.contains(endWord)) {
+            return 0;
+        }
 
-}
+        q.add(new Pair(beginWord, 1));
 
-return 0;
-        
-    }
-    public boolean check(String current, String front){
-    int count=0;
-    for(int i=0;i<current.length();i++){
-        if(current.charAt(i)!=front.charAt(i))count++;
-    }
-    return count==1;
-}
+        while (!q.isEmpty()) {
+            Pair curr = q.remove();
 
+            if (curr.s.equals(endWord)) {
+                return curr.seqLen;
+            }
+
+            for (int i = 0; i < curr.s.length(); i++) {
+                char[] charArray = curr.s.toCharArray();
+
+                for (char j = 'a'; j <= 'z'; j++) {
+                    charArray[i] = j;
+                    String f = new String(charArray);
+
+                    if (f.equals(beginWord)) {
+                        continue;
+                    }
+
+                    if (list.contains(f)) {
+                        q.add(new Pair(f, curr.seqLen + 1));
+                        list.remove(f);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
 }

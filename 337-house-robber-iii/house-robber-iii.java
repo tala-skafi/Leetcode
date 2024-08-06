e268cb0c71e9d2b09d86ffb53f4efb5b2fa21a4d
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2024-08-06 22:40:28 +0300

    Time: 3 ms (18.16%) | Memory: 45.6 MB (5.76%) - LeetSync

---
+++ 337-house-robber-iii/house-robber-iii.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map <TreeNode,Integer> mapfalse;
+    Map <TreeNode,Integer> maptrue;
+    public int rob(TreeNode root) {
+        mapfalse=new HashMap<>();
+        maptrue=new HashMap<>();
+
+        return DFS(root,false);
+        
+    }
+    private int DFS(TreeNode root, boolean flag){
+        if(root==null)return 0;
+        int res=0;
+        if(flag && maptrue.containsKey(root)) {
+            return maptrue.get(root);
+        }
+        if(!flag && mapfalse.containsKey(root)) {
+            return mapfalse.get(root);
+        }
+        if(!flag){
+            res= Math.max(root.val+DFS(root.right,true)+DFS(root.left,true),DFS(root.right,false)+DFS(root.left,false));
+        }
+         
+         else{
+            res= DFS(root.right,false)+DFS(root.left,false);
+         }
+           if(flag){
+            maptrue.put(root, res);
+            }
+            else{
+                 mapfalse.put(root, res);
+            }
+            return res;
+         
+    }
+    
+}

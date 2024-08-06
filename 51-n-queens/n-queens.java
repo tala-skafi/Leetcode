e3df900227bb1bcf75ae3c9e3d5e84421d5a2c0f
tala-skafi <65433042+tala-skafi@users.noreply.github.com>
2024-08-06 18:10:35 +0300

    Time: 2 ms (89.29%) | Memory: 44.8 MB (55.39%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -5,48 +5,37 @@ class Solution {
     public List<List<String>> solveNQueens(int n) {
         res = new ArrayList<>();
         N = n;
-        int[] queens = new int[n];  // Array to store the position of queens in each row
-        Arrays.fill(queens, -1);
-        DFS(0, queens, new HashSet<>(), new HashSet<>(), new HashSet<>());
+        DFS(n, new int[n], new ArrayList<>(), new boolean[n], new boolean[2 * n], new boolean[2 * n]);
         return res;
     }
 
-    private void DFS(int row, int[] queens, Set<Integer> cols, Set<Integer> diags1, Set<Integer> diags2) {
-        if (row == N) {
-            res.add(generateBoard(queens));
+    private void DFS(int n, int[] info, List<String> temp, boolean[] cols, boolean[] d1, boolean[] d2) {
+        if (n == 0) {
+            res.add(new ArrayList<>(temp));
             return;
         }
 
-        for (int col = 0; col < N; col++) {
-            int diag1 = row - col;
-            int diag2 = row + col;
+        int row = N - n;
+        char[] rowArray = new char[N];
+        Arrays.fill(rowArray, '.');
 
-            if (cols.contains(col) || diags1.contains(diag1) || diags2.contains(diag2)) {
-                continue;
-            }
-
-            queens[row] = col;
-            cols.add(col);
-            diags1.add(diag1);
-            diags2.add(diag2);
-
-            DFS(row + 1, queens, cols, diags1, diags2);
-
-            queens[row] = -1;
-            cols.remove(col);
-            diags1.remove(diag1);
-            diags2.remove(diag2);
-        }
-    }
-
-    private List<String> generateBoard(int[] queens) {
-        List<String> board = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            char[] row = new char[N];
-            Arrays.fill(row, '.');
-            row[queens[i]] = 'Q';
-            board.add(new String(row));
+            if (!cols[i] && !d1[row - i + N] && !d2[row + i]) {
+                rowArray[i] = 'Q';
+                temp.add(new String(rowArray));
+                cols[i] = true;
+                d1[row - i + N] = true;
+                d2[row + i] = true;
+
+                DFS(n - 1, info, temp, cols, d1, d2);
+
+                // Backtrack
+                temp.remove(temp.size() - 1);
+                cols[i] = false;
+                d1[row - i + N] = false;
+                d2[row + i] = false;
+                rowArray[i] = '.';
+            }
         }
-        return board;
     }
 }
